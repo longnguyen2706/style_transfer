4cf9007e9d4b9ec0789f3d7209b96c89e3b5e0e0
Louis Nguyen <ndlong95@gmail.com>
2023-12-02 23:24:14 -0500

    added change by tony

---
+++ config/avg_original_config.py
@@ -0,0 +1,8 @@
+num_steps=1000
+style_weight=5e6
+content_weight=1
+content_layers=['conv_4']
+style_layers=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+tv_weight = 0.0
+optimizer_choice='lbfgs'
+loss_choice='generic'
